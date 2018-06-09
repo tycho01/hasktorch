995e1abfd417468ea0d0c0052ddd34edaecf6352
stites <sam@stites.io>
2018-06-09 14:10:28 -0400

    check in haddocks

---
+++ hasktorch-signatures-types-0.1.0.0/build/autogen/Paths_hasktorch_signatures_types.hs
@@ -0,0 +1,50 @@
+{-# LANGUAGE CPP #-}
+{-# LANGUAGE NoRebindableSyntax #-}
+{-# OPTIONS_GHC -fno-warn-missing-import-lists #-}
+module Paths_hasktorch_signatures_types (
+    version,
+    getBinDir, getLibDir, getDynLibDir, getDataDir, getLibexecDir,
+    getDataFileName, getSysconfDir
+  ) where
+
+import qualified Control.Exception as Exception
+import Data.Version (Version(..))
+import System.Environment (getEnv)
+import Prelude
+
+#if defined(VERSION_base)
+
+#if MIN_VERSION_base(4,0,0)
+catchIO :: IO a -> (Exception.IOException -> IO a) -> IO a
+#else
+catchIO :: IO a -> (Exception.Exception -> IO a) -> IO a
+#endif
+
+#else
+catchIO :: IO a -> (Exception.IOException -> IO a) -> IO a
+#endif
+catchIO = Exception.catch
+
+version :: Version
+version = Version [0,1,0,0] []
+bindir, libdir, dynlibdir, datadir, libexecdir, sysconfdir :: FilePath
+
+bindir     = "/home/stites/.cabal/bin"
+libdir     = "/home/stites/.cabal/lib/x86_64-linux-ghc-8.4.2/hasktorch-signatures-types-0.1.0.0-inplace"
+dynlibdir  = "/home/stites/.cabal/lib/x86_64-linux-ghc-8.4.2"
+datadir    = "/home/stites/.cabal/share/x86_64-linux-ghc-8.4.2/hasktorch-signatures-types-0.1.0.0"
+libexecdir = "/home/stites/.cabal/libexec/x86_64-linux-ghc-8.4.2/hasktorch-signatures-types-0.1.0.0"
+sysconfdir = "/home/stites/.cabal/etc"
+
+getBinDir, getLibDir, getDynLibDir, getDataDir, getLibexecDir, getSysconfDir :: IO FilePath
+getBinDir = catchIO (getEnv "hasktorch_signatures_types_bindir") (\_ -> return bindir)
+getLibDir = catchIO (getEnv "hasktorch_signatures_types_libdir") (\_ -> return libdir)
+getDynLibDir = catchIO (getEnv "hasktorch_signatures_types_dynlibdir") (\_ -> return dynlibdir)
+getDataDir = catchIO (getEnv "hasktorch_signatures_types_datadir") (\_ -> return datadir)
+getLibexecDir = catchIO (getEnv "hasktorch_signatures_types_libexecdir") (\_ -> return libexecdir)
+getSysconfDir = catchIO (getEnv "hasktorch_signatures_types_sysconfdir") (\_ -> return sysconfdir)
+
+getDataFileName :: FilePath -> IO FilePath
+getDataFileName name = do
+  dir <- getDataDir
+  return (dir ++ "/" ++ name)
