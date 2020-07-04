84db3057ca6d0cfc121ad5f68a8d841b93893d78
Kiara Grouwstra <tycho01@pm.me>
2020-07-04 12:55:08 +0200

    reinstate Hint extensions

--- synthesis/library/Synthesis/Hint.hs
+++ synthesis/library/Synthesis/Hint.hs
@@ -40,7 +40,10 @@ interpretUnsafe fn = join $
 -- | run an interpreter monad with imports
 interpretSafe :: Interpreter a -> IO (Either InterpreterError a)
 interpretSafe fn = runInterpreter $ do
-  set [languageExtensions := [ RankNTypes ]]
+  set [languageExtensions := [ PartialTypeSignatures
+                             , ScopedTypeVariables
+                             , RankNTypes
+                             ]]
   setImportsF imports
     >>= const fn
 
