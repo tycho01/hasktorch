f9cf958b3e8c7b8a5c330dabce5eca823b96ac9f
Kiara Grouwstra <tycho01@pm.me>
2020-07-06 11:22:17 +0200

    outsource distributions

--- synthesis/library/Synthesis/Synthesizer/Exports.hs
+++ synthesis/library/Synthesis/Synthesizer/Exports.hs
@@ -1,11 +1,6 @@
 module Synthesis.Synthesizer.Exports (
     module Synthesis.Synthesizer.Exports
-    -- hasktorch
-    , module Synthesis.Synthesizer.Constraints
-    , module Synthesis.Synthesizer.Distribution
-    , module Synthesis.Synthesizer.Categorical
     , module Synthesis.Synthesizer.UntypedMLP
-    -- mine
     , module Synthesis.Synthesizer.Utility
     , module Synthesis.Synthesizer.Encoder
     , module Synthesis.Synthesizer.R3NN
@@ -16,12 +11,7 @@ module Synthesis.Synthesizer.Exports (
     , module Synthesis.Synthesizer.Train
 ) where
 
--- hasktorch
-import Synthesis.Synthesizer.Categorical
-import Synthesis.Synthesizer.Distribution
-import Synthesis.Synthesizer.Constraints
 import Synthesis.Synthesizer.UntypedMLP
--- mine
 import Synthesis.Synthesizer.Utility
 import Synthesis.Synthesizer.Encoder
 import Synthesis.Synthesizer.R3NN
