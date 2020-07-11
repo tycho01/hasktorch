ef3500eefddb6f3d39a6b2563263c41664fd3e75
Kiara Grouwstra <tycho01@pm.me>
2020-07-11 11:29:39 +0200

    identify mem leak culprits

--- synthesis/synth-gen/library/Synthesis/Generator.hs
+++ synthesis/synth-gen/library/Synthesis/Generator.hs
@@ -15,7 +15,7 @@ import Data.Text (Text)
 import qualified Data.ByteString as BS
 import Data.ByteString.Char8 (pack)
 import Data.ByteString.Lazy (toStrict, fromStrict)
-import Data.Bifunctor (first)
+import Data.Bifunctor (first, bimap)
 import Data.HashMap.Lazy
   ( (!),
     HashMap,
@@ -133,6 +133,7 @@ main = do
         pb <- newProgressBar pgStyle 1 (Progress 0 (size fn_type_instantiations) "generator")
         writeFile jsonLinesPath ""
         forM_ (toList fn_type_instantiations) $ \(fn, type_instantiations) -> do
+            notice_ $ "loop: " <> show (pp fn, bimap (fmap pp) pp <$> type_instantiations)
             (`finally` incProgress pb 1) . join $ BS.appendFile jsonLinesPath . (<> pack "\n") . toStrict . Aeson.encode . (fn,) <.> interpretUnsafe $ fnOutputs crashOnError maxParams both_instantiation_inputs fn type_instantiations
 
     fn_type_ios :: HashMap Expr (HashMap (Tp, Tp) [(Expr, Either String Expr)]) <- fromList . fmap (fromJust . Aeson.decode . fromStrict . pack) . init . lines <$> readFile jsonLinesPath
