ce01d1cff189655469274117423932972dfee45a
Kiara Grouwstra <tycho01@pm.me>
2020-07-12 13:42:24 +0200

    fix more refs

--- synthesis/synthesizer/synthesizer/Main.hs
+++ synthesis/synthesizer/synthesizer/Main.hs
@@ -2,7 +2,7 @@
 -- It is generally a good idea to keep all your business logic in your library
 -- and only use it in the executable. Doing so allows others to use what you
 -- wrote in their libraries.
-import qualified Synthesis.Synthesize
+import qualified Synthesizer.Synthesize
 
 main ∷ IO ()
-main = Synthesis.Synthesize.main
+main = Synthesizer.Synthesize.main
