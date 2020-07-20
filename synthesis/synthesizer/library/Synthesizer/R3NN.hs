f16281352ac6ba0510695aa3800aadf6d12b85f3
Kiara Grouwstra <tycho01@pm.me>
2020-07-20 15:06:49 +0200

    halve params

--- synthesis/synthesizer/library/Synthesizer/R3NN.hs
+++ synthesis/synthesizer/library/Synthesizer/R3NN.hs
@@ -68,7 +68,6 @@ data R3NNSpec
         -- symbolIdxs :: HashMap String Int
         -- ppt :: Expr
       . { variant_sizes :: HashMap String Int
-        , conditionSpec :: LSTMSpec (m + batch_size * maxStringLength * (2 * featMult * Dirs * h)) (Div m Dirs) NumLayers Dir 'D.Float device
         , scoreSpec     :: LSTMSpec  m                                                             (Div m Dirs) NumLayers Dir 'D.Float device
         }
         -> R3NNSpec device m symbols rules maxStringLength batch_size h numChars featMult
@@ -87,17 +86,9 @@ data R3NN
     -- I imagine NSPS fixed their batch to the sample size, but I have those for each type instantiation, making this harder for me to fix. as a work-around, I'm sampling instead.
  where
     R3NN :: forall m symbols rules maxStringLength batch_size device h numChars featMult
-      . { condition_model :: LSTMWithInit (m + batch_size * maxStringLength * (2 * featMult * Dirs * h)) (Div m Dirs) NumLayers Dir 'ConstantInitialization 'D.Float device
-        , score_model     :: LSTMWithInit  m                                    (Div m Dirs) NumLayers Dir 'ConstantInitialization 'D.Float device
-        -- NSPS: for each production rule r∈R, a nnet f_r from x∈R^(Q⋅M) to y∈R^M,
-        -- with Q as the number of symbols on the RHS of the production rule r.
+      . { score_model     :: LSTMWithInit  m                                    (Div m Dirs) NumLayers Dir 'ConstantInitialization 'D.Float device
         , left_nnets :: HashMap String MLP
-        -- for each production rule r∈R, a nnet g_r from x'∈R^M to y'∈R^(Q⋅M).
-        , right_nnets :: HashMap String MLP
-        -- for each symbol s∈S, an M-dimensional representation ϕ(s)∈R^M.
         , symbol_emb :: Parameter device 'D.Float '[symbols, m]
-        -- for each production rule r∈R, an M−dimensional representation: ω(r)∈R^M.
-        , rule_emb   :: Parameter device 'D.Float '[rules  , m]
         }
         -> R3NN device m symbols rules maxStringLength batch_size h numChars featMult
  deriving (Show, Generic)
@@ -129,23 +120,13 @@ instance ( KnownDevice device
  where
     sample R3NNSpec {..} = do
         join . return $ R3NN
-            -- condition_model
-            <$> A.sample (LSTMWithZerosInitSpec conditionSpec)
-            -- score_model
             <*> A.sample (LSTMWithZerosInitSpec scoreSpec)
-            -- left: untyped as q is not static
             <*> mapM (\q -> A.sample $ MLPSpec (q * m) m) variant_sizes
-            -- right: ditto
-            <*> mapM (\q -> A.sample $ MLPSpec m (q * m)) variant_sizes
-            -- symbol_emb
             <*> (fmap UnsafeMkParameter . D.makeIndependent =<< D.randnIO' [symbols, m])
-            -- rule_emb
-            <*> (fmap UnsafeMkParameter . D.makeIndependent =<< D.randnIO' [rules,   m])
             where
                 -- m must be divisible by Dirs for `Div` in the LSTM specs to work out due to integer division...
                 m = assertP ((== 0) . (`mod` natValI @Dirs)) $ natValI @m
                 symbols = natValI @symbols
-                rules = natValI @rules
 
 -- | initialize R3NN spec
 initR3nn :: forall m symbols rules maxStringLength batch_size h device numChars featMult
@@ -157,17 +138,8 @@ initR3nn :: forall m symbols rules maxStringLength batch_size h device numChars 
          -> (R3NNSpec device m symbols rules maxStringLength batch_size h numChars featMult)
 initR3nn variants batch_size dropoutRate charMap = R3NNSpec @device @m @symbols @rules @maxStringLength @batch_size @h @numChars @featMult
         variant_sizes
-        -- condition
-        (LSTMSpec $ DropoutSpec dropoutRate)
-        -- score
         (LSTMSpec $ DropoutSpec dropoutRate)
     where
-        maxStringLength :: Int = natValI @maxStringLength
-        m :: Int = natValI @m
-        h :: Int = natValI @h
-        featMult :: Int = natValI @featMult 
-        -- TODO: can I really cram all that back into just M?
-        conditionIn = m + batch_size * 2 * featMult * dirs * h * maxStringLength
         variant_sizes :: HashMap String Int = fromList $ variantInt . snd <$> variants
 
 variantInt :: Expr -> (String, Int)
@@ -179,9 +151,6 @@ patchR3nnLoss r3nn_model variant_sizes = let
         dropoutOn = True
         m :: Int = natValI @m
         left_dummy  :: Tensor device 'D.Float '[] = mulScalar (0.0 :: Float) $ sumAll $ Torch.Typed.Tensor.toDType @'D.Float . UnsafeMkTensor $ F.cat (F.Dim 1) $ fmap (\(k,mlp_) -> let q = safeIndexHM variant_sizes k in mlp mlp_ $ D.zeros' [1,q*m]) $ toList $  left_nnets r3nn_model
-        right_dummy :: Tensor device 'D.Float '[] = mulScalar (0.0 :: Float) $ sumAll $ Torch.Typed.Tensor.toDType @'D.Float . UnsafeMkTensor $ F.cat (F.Dim 1) $ fmap (\   mlp_  ->                              mlp mlp_ $ D.zeros' [1,  m]) $ elems  $ right_nnets r3nn_model
-        condition_dummy :: Tensor device 'D.Float '[] = mulScalar (0.0 :: Float) $ sumAll $ fstOf3 . lstmDynamicBatch @'SequenceFirst dropoutOn (condition_model r3nn_model) $ (ones :: Tensor device 'D.Float '[1,1,(m + batch_size * maxStringLength * (2 * featMult * Dirs * h))])
         score_dummy :: Tensor device 'D.Float '[] = mulScalar (0.0 :: Float) $ sumAll $ fstOf3 . lstmDynamicBatch @'SequenceFirst dropoutOn (score_model r3nn_model) $ (ones :: Tensor device 'D.Float '[1,1,m])
         symbol_dummy :: Tensor device 'D.Float '[] = mulScalar (0.0 :: Float) $ sumAll $ Torch.Typed.Parameter.toDependent $ (symbol_emb r3nn_model)
-        rule_dummy   :: Tensor device 'D.Float '[] = mulScalar (0.0 :: Float) $ sumAll $ Torch.Typed.Parameter.toDependent $ (rule_emb   r3nn_model)
-    in add $ Torch.Typed.Tensor.toDevice $ left_dummy `add` right_dummy `add` condition_dummy `add` score_dummy `add` symbol_dummy `add` rule_dummy
+    in add $ Torch.Typed.Tensor.toDevice $ left_dummy `add` score_dummy `add` symbol_dummy
