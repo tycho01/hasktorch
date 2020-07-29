bb8063d1bb683e3a7c7d1bab4e17a906cdbdddca
Kiara Grouwstra <tycho01@pm.me>
2020-07-29 16:25:43 +0200

    0 epoch, propagate Interpreter to combat mem leak

--- synthesis/synth-gen/library/Synthesis/Data.hs
+++ synthesis/synth-gen/library/Synthesis/Data.hs
@@ -264,7 +264,7 @@ combineConfig optCfg hparComb = cfg
                 , cheat                = cheat
                 , gpu                  = gpu
                 , savedModelPath       = ""
-                , initialEpoch         = (1 :: Int)
+                , initialEpoch         = (0 :: Int)
                 }
 
 data PreppedDSL = PreppedDSL
